020b75bff4200be3c14eb42b9d0d3cbcad309d84
minkiapps <minkiapps@gmail.com>
2020-10-18 15:02:43 +0200

    add agc crash service

--- app/src/main/java/com/minkiapps/livetranslator/App.kt
+++ app/src/main/java/com/minkiapps/livetranslator/App.kt
@@ -2,6 +2,7 @@ package com.minkiapps.livetranslator
 
 import android.app.Application
 import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.agconnect.crash.AGConnectCrash
 import com.huawei.hms.mlsdk.common.MLApplication
 import timber.log.Timber
 
@@ -9,6 +10,8 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        AGConnectCrash.getInstance().enableCrashCollection(!BuildConfig.DEBUG)
 
         if(BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
